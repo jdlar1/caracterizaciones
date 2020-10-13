988745fbba0e1fb06ed51dbece598bff7fcb10bf
jdlar1 <58822427+jdlar1@users.noreply.github.com>
2020-10-12 20:20:24 -0500

    Añadido generador de diente de sierra

--- comparador/comparador.ino
+++ comparador/comparador.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+int diente_de_sierra = 11;
+int frecuencia = 6;
 
+float sawtooth(){
+  float a = float(frecuencia*(millis() % (int(1000/frecuencia))))*0.255;
+  return a;
+}
+
+void setup() {
+ Serial.begin(112500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float a = sawtooth();
+  Serial.println(a);
 }
